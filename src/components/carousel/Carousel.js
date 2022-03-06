b194d85844ffb91c33ddc2614c6be229497e32c4
Riley Johnston <rileydanejohnston@gmail.com>
2022-03-06 15:55:14 -0600

    feat: add rough draft of slide

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,17 +1,75 @@
 import React from 'react'
 import arrowLeft from '../../images/arrow-left.svg'
 import arrowRight from '../../images/arrow-right.svg'
+import container0 from '../../images/container-0.png'
 import {
   CarouselWrapper,
   CarouselButton,
-  Slide
+  SlideWrapper,
+  SlideImg,
+  SlideDetails,
+  ListTitle,
+  List,
+  Item,
+  ListGroup
 } from './styledCarousel'
 
 const Carousel = () => {
+
+  const ContainerData = {
+    image: container0,
+    altText: 'Stainless steel containers image 1',
+    features: [
+      'Removable/adjustable divider',
+      'Leak-resistant lid keeps food fresh',
+      'Rectangle shapes stack to save storage space',
+      'Easy to sanitize and dishwasher safe',
+      'Containers and lids are freezer safe',
+      'Containers are oven safe up to 300 degrees',
+      'Lighter and sturdier than glass',
+      'Etched tare weights for easy bulk shopping',
+      '304 (18/8) stainless steel, LDPE #4 plastic lid, PP #5 plastic divider',
+      'Free of BPA, BPS, phthalates and lead'
+    ],
+    sizing: [
+      '25oz container size with lid on: 7.75” x 1.75" x 5” Lids: 7.75” x 5”'
+    ]
+  }
+  /*
+
+*/
   return (
     <CarouselWrapper >
       <CarouselButton image={arrowLeft} />
-      <Slide />
+      <SlideWrapper>
+        <SlideImg src={container0} alt='Stainless steel containers image 1'/>
+        <SlideDetails>
+          <ListGroup>
+            <ListTitle>Features</ListTitle>
+            <List>
+              {
+                ContainerData.features.map((feature, index) => {
+                  return (
+                    <Item key={index}>{feature}</Item>
+                  );
+                })
+              }
+            </List>
+          </ListGroup>
+          <ListGroup>
+            <ListTitle>Sizing</ListTitle>
+            <List>
+              {
+                ContainerData.sizing.map((size, index) => {
+                  return (
+                    <Item key={index}>{size}</Item>
+                  );
+                })
+              }
+            </List>
+          </ListGroup>
+        </SlideDetails>
+      </SlideWrapper>
       <CarouselButton image={arrowRight} />
     </CarouselWrapper>
   )
--- src/components/carousel/styledCarousel.js
+++ src/components/carousel/styledCarousel.js
@@ -1,4 +1,5 @@
 import styled from "styled-components/macro";
+import bulletImg from '../../images/bullet-point.svg';
 
 export const CarouselWrapper = styled.div`
   display: flex;
@@ -6,11 +7,52 @@ export const CarouselWrapper = styled.div`
   align-items: center;
 `;
 
-export const Slide = styled.div`
+export const SlideWrapper = styled.div`
   width: 90.45%;       /* 1212 width / 1340 width */
-  height: 600px;
   background: #fff;
   box-shadow: 0px 2px 10px 0px #00000033;
+
+  display: flex;
+  gap: 6.6%;            /* 80px gap / 1212px width */
+  padding: 40px 10px;   /* fudging these numbers a bit for convenience*/
+`;
+
+export const SlideImg = styled.img`
+/* 
+    520px gap / 1192px width
+    1212 - 20 padding = 1192
+*/
+  width: 43.62%;
+`;
+
+export const SlideDetails = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const ListGroup = styled.div``;
+
+export const ListTitle = styled.h3`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: 0em;
+  margin-bottom: 16px;
+`;
+
+export const List = styled.ul`
+  list-style-image: url(${bulletImg});
+  padding-left: 14px;
+`;
+
+export const Item = styled.li`
+  padding-left: 10px; 
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 30px;
+  letter-spacing: 0em;
 `;
 
 export const CarouselButton = styled.button`
