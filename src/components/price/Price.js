4274356a7131d4527d1a3805cb1cd0f991864616
Riley Johnston <rileydanejohnston@gmail.com>
2022-03-30 01:28:26 -0500

    feat: add responsive designs

--- src/components/price/Price.js
+++ src/components/price/Price.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import { Image, PriceContent, PriceWrapper, Text, Title, WorkButton } from './styledPrice'
+import { Image, PriceWrapper, Text, Title, WorkButton } from './styledPrice'
 import { PriceData } from '../../constants/priceConst'
 const { title, text, button } = PriceData;
 
 const Price = (props) => {
   return (
     <PriceWrapper>
-      <PriceContent>
-        <Title>{title}</Title>
-        <Image />
-        <WorkButton
-          onClick={props.openPopup}>
-          {button}
-        </WorkButton>
-        <Text>{text}</Text>
-      </PriceContent>
+      <Title>{title}</Title>
+      <Image />
+      <WorkButton
+        onClick={props.openPopup}>
+        {button}
+      </WorkButton>
+      <Text>{text}</Text>
     </PriceWrapper>
   )
 }
--- src/constants/priceConst.js
+++ src/constants/priceConst.js
@@ -1,5 +1,5 @@
 export const PriceData = {
-  title: `Get in touch with us to calculate prices\nfor your business`,
+  title: `Get in touch with us to calculate prices for your business`,
   text: 'Leave your contacts and request, and our manager will reach out to you to establish our futher partnership!',
   button: 'Work with us'
 };
